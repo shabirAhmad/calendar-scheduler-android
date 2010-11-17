c83d08654ef5199623aeb3d251a24cc14ad52fb1
nivco@google.com <nivco@google.com@6a89b35a-1051-2f51-6090-0785da90576f>
2010-11-17 01:00:40 +0000

    Layout of the attendee selection Activity.

---
+++ trunk/src/com/google/android/apps/meetingscheduler/Attendee.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.android.apps.meetingscheduler;
+
+import android.widget.ImageView;
+
+/**
+ * Represent an attendee or a potential attendee to a meeting.
+ *
+ * @since 2.2
+ * @author Nicolas Garnier (nivco@google.com)
+ */
+public class Attendee {
+
+  /** Photo of the participant */
+  public ImageView photo;
+
+  /** Display name of the participant */
+  public String name;
+
+  /** email of the calendar of the participant */
+  public String email;
+
+  /**
+   * Default Constructor.
+   */
+  public Attendee() {
+  }
+
+  /**
+   * Constructor that initializes the attributes.
+   *
+   * @param name The name of the attendee
+   * @param email The email of the calendar of the attendee
+   * @param photo The photo of the attendee
+   */
+  public Attendee(String name, String email, ImageView photo){
+    this.name = name;
+    this.email = email;
+    this.photo = photo;
+  }
+}
---
+++ trunk/src/com/google/android/apps/meetingscheduler/MockAttendeeRetriever.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.android.apps.meetingscheduler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Mock Attendee Retriever just returns a list of attendees manually build for
+ * testing.
+ *
+ * @since 2.2
+ * @author Nicolas Garnier (nivco@google.com)
+ */
+public class MockAttendeeRetriever implements AttendeeRetriever {
+
+  /**
+   * Returns the list of attendees selectable.
+   *
+   * @return The list of selectable attendees
+   */
+  public List<Attendee> getPossibleAttendees() {
+    List<Attendee> attendees = new ArrayList<Attendee>();
+    attendees.add(new Attendee("foo", "foo@gmail.com", null));
+    attendees.add(new Attendee("bar bar bar bar bar bar bar bar bar bar", "bar@gmail.com", null));
+    attendees.add(new Attendee("biz", "biz@gmail.com", null));
+    attendees.add(new Attendee("liz", "liz@gmail.com", null));
+    attendees.add(new Attendee("bob", "bob@gmail.com", null));
+    attendees.add(new Attendee("zob", "zob@gmail.com", null));
+    attendees.add(new Attendee("luc", "luc@gmail.com", null));
+    attendees.add(new Attendee("alf", "alf@gmail.com", null));
+    attendees.add(new Attendee("nic", "nic@gmail.com", null));
+    return attendees;
+  }
+}
