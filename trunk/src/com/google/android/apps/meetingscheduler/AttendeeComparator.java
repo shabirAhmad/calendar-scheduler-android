86829a870225f7812c8396a8d6cc5b954031c10f
nivco@google.com <nivco@google.com@6a89b35a-1051-2f51-6090-0785da90576f>
2010-12-02 16:25:22 +0000

    unneeded comment

--- trunk/src/com/google/android/apps/meetingscheduler/AttendeeComparator.java
+++ trunk/src/com/google/android/apps/meetingscheduler/AttendeeComparator.java
@@ -4,10 +4,10 @@ package com.google.android.apps.meetingscheduler;
 import java.util.Comparator;
 
 /**
- * 
+ *
  * Comparator use to sort a list of attendee. The attendees are sort
  * alphabetically and from selected to unselected.
- * 
+ *
  * @author Alain Vongsouvanh (alainv@google.com)
  */
 public class AttendeeComparator implements Comparator<Attendee> {
@@ -18,9 +18,6 @@ public class AttendeeComparator implements Comparator<Attendee> {
   public static final AttendeeComparator Comparator = new AttendeeComparator();
 
   @Override
-  /**
-   * Compare 2 attendees.
-   */
   public int compare(Attendee lhs, Attendee rhs) {
     if (lhs.selected == rhs.selected)
       return lhs.name.compareToIgnoreCase(rhs.name);
