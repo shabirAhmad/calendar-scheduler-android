88765ec1a561162bef2267bc223a7637936fbb29
alainv <alainv@google.com>
2010-12-02 20:58:54 +0000

    Added a meeting start-time/end-time selection dialog before creating event.

--- src/com/google/android/apps/meetingscheduler/EventExpandableListAdapter.java
+++ src/com/google/android/apps/meetingscheduler/EventExpandableListAdapter.java
@@ -16,14 +16,17 @@
 
 package com.google.android.apps.meetingscheduler;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.text.format.DateUtils;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.Button;
 import android.widget.TextView;
@@ -41,7 +44,7 @@ import java.util.TimeZone;
 
 /**
  * Adapts the Meeting data to the ExpendableListView.
- *
+ * 
  * @author Nicolas Garnier
  */
 public class EventExpandableListAdapter extends BaseExpandableListAdapter {
@@ -58,26 +61,31 @@ public class EventExpandableListAdapter extends BaseExpandableListAdapter {
   /** Inflater used to create Views from layouts */
   private LayoutInflater inflater;
 
+  /** The length of the meeting */
+  private int meetingLength;
+
   /**
    * Constructs a new EventExpandableListAdapter given the List of Dates
-   *
+   * 
    * @param context The context of the application
    * @param availableMeetingTimes All the times for which a meeting is possible
    *          for the attendees
    */
   public EventExpandableListAdapter(Context context,
-      List<AvailableMeetingTime> availableMeetingTimes) {
+      List<AvailableMeetingTime> availableMeetingTimes, int meetingLength) {
     this.context = context;
 
     sortedEventsByDays = sortEventsByDay(availableMeetingTimes);
     sortedDays = asSortedList(sortedEventsByDays.keySet());
 
     inflater = LayoutInflater.from(context);
+
+    this.meetingLength = meetingLength;
   }
 
   /**
    * Sorts a Collection and returns it as a Sorted List.
-   *
+   * 
    * @param c the collection to sort
    * @return The sorted collection as a List
    */
@@ -90,7 +98,7 @@ public class EventExpandableListAdapter extends BaseExpandableListAdapter {
   /**
    * Parses the lit of AvailableMeetingTime to map then by day they take place
    * in the current time zone.
-   *
+   * 
    * @param availableMeetingTimes The list of AvailableMeetingTime
    * @return lists of AvailableMeetingTime mapped by day they take place
    */
@@ -143,31 +151,57 @@ public class EventExpandableListAdapter extends BaseExpandableListAdapter {
     final Date startDate = getChild(groupPosition, childPosition).start;
     final Date endDate = getChild(groupPosition, childPosition).end;
     final List<Attendee> attendees = getChild(groupPosition, childPosition).attendees;
-    String dateStart = DateUtils.formatDateTime(context, startDate.getTime(),
-        DateUtils.FORMAT_SHOW_TIME);
-    String dateEnd = DateUtils.formatDateTime(context, endDate.getTime(),
-        DateUtils.FORMAT_SHOW_TIME);
-    text.setText(dateStart + " - " + dateEnd);
+    text.setText(getMeetingDisplayString(startDate, endDate));
     // Adding Action to button
     Button button = (Button) view.findViewById(R.id.meeting_time_create_button);
     button.setOnClickListener(new OnClickListener() {
       public void onClick(View v) {
-        // TODO: fire an intent of the Google Calendar App to create an event.
-        //       If the Calendar App is not installed fire the intent below
-        //       which redirects to the Web UI to create an event.
-        String attendeesEmails = attendees.get(0).email;
-        for (int i = 1; i < attendees.size(); i++) {
-          attendeesEmails += "," + attendees.get(i).email;
+        final List<Pair<Date, Date>> calendars = getPossibleStartingTime(startDate, endDate);
+
+        if (calendars.size() > 1) {
+          final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+          builder.setTitle(R.string.choose_meeting_time);
+          builder.setCancelable(true);
+          builder.setNegativeButton(R.string.cancel, null);
+          builder.setItems(getPossibleStartingTimeDisplay(calendars),
+              new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                  Pair<Date, Date> meeting = calendars.get(which);
+                  createEvent(meeting.first, meeting.second, attendees);
+                }
+              });
+          builder.show();
+        } else
+          createEvent(startDate, endDate, attendees);
+
+      }
+
+      private List<Pair<Date, Date>> getPossibleStartingTime(Date startDate, Date endDate) {
+        List<Pair<Date, Date>> result = new ArrayList<Pair<Date, Date>>();
+        Calendar currentStart = new GregorianCalendar();
+        Calendar currentEnd = new GregorianCalendar();
+
+        currentStart.setTime(startDate);
+        currentEnd.setTime(startDate);
+        currentEnd.add(Calendar.MINUTE, meetingLength);
+        while (!currentEnd.getTime().after(endDate)) {
+          result.add(new Pair<Date, Date>(currentStart.getTime(), currentEnd.getTime()));
+          currentStart.add(Calendar.MINUTE, 15);
+          currentEnd.add(Calendar.MINUTE, 15);
         }
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'");
-        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-        String createEventUrl = "https://www.google.com/calendar/render?" + "action=TEMPLATE"
-            + "&dates=" + sdf.format(startDate) + "/" + sdf.format(endDate) + "&add="
-            + attendeesEmails + "&crm=BUSY" + "&gsessionid=OK" + "&sf=true" + "&output=xml";
-        Intent browse = new Intent(Intent.ACTION_VIEW, Uri.parse(createEventUrl));
+        return result;
+      }
 
-        context.startActivity(browse);
+      private String[] getPossibleStartingTimeDisplay(List<Pair<Date, Date>> meetings) {
+        String[] result = new String[meetings.size()];
+        int i = 0;
+
+        for (Pair<Date, Date> meeting : meetings) {
+          result[i++] = getMeetingDisplayString(meeting.first, meeting.second);
+        }
+
+        return result;
       }
     });
     return view;
@@ -200,6 +234,41 @@ public class EventExpandableListAdapter extends BaseExpandableListAdapter {
 
   public boolean hasStableIds() {
     return true;
+  }
+
+  private String getMeetingDisplayString(Date startDate, Date endDate) {
+    String dateStart = DateUtils.formatDateTime(context, startDate.getTime(),
+        DateUtils.FORMAT_SHOW_TIME);
+    String dateEnd = DateUtils.formatDateTime(context, endDate.getTime(),
+        DateUtils.FORMAT_SHOW_TIME);
+
+    return dateStart + " - " + dateEnd;
+  }
+
+  /**
+   * Creates an event using the Calendar API with the selected attendees.
+   * 
+   * @param startDate The start date of the event.
+   * @param endDate The end date of the event.
+   * @param attendees The attendees to add to the event.
+   */
+  private void createEvent(final Date startDate, final Date endDate, final List<Attendee> attendees) {
+    // TODO: fire an intent of the Google Calendar App to create an event.
+    // If the Calendar App is not installed fire the intent below
+    // which redirects to the Web UI to create an event.
+    String attendeesEmails = attendees.get(0).email;
+    for (int i = 1; i < attendees.size(); i++) {
+      attendeesEmails += "," + attendees.get(i).email;
+    }
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'");
+    sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+    String createEventUrl = "https://www.google.com/calendar/render?" + "action=TEMPLATE"
+        + "&dates=" + sdf.format(startDate) + "/" + sdf.format(endDate) + "&add=" + attendeesEmails
+        + "&crm=BUSY" + "&gsessionid=OK" + "&sf=true" + "&output=xml";
+    Intent browse = new Intent(Intent.ACTION_VIEW, Uri.parse(createEventUrl));
+
+    context.startActivity(browse);
   }
 
 }
