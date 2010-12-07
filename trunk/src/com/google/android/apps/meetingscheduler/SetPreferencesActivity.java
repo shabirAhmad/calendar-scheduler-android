628dd6fd2601878fa80dba4b4ab0a5f895f33745
prashantt@google.com <prashantt@google.com@6a89b35a-1051-2f51-6090-0785da90576f>
2010-12-07 10:17:45 +0000

    Cosmetic fixes to the basic menu code.

--- trunk/src/com/google/android/apps/meetingscheduler/SetPreferencesActivity.java
+++ trunk/src/com/google/android/apps/meetingscheduler/SetPreferencesActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.google.android.apps.meetingscheduler;
 
 import android.content.Context;
@@ -5,11 +21,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
+/**
+ * Activity screen to show and set the app preferences
+ * 
+ * @author Prashant Tiwari
+ */
 public class SetPreferencesActivity extends PreferenceActivity {
+  
   /**
+   * Returns an Intent that will display this Activity.
    * 
-   * @param context
-   * @return
+   * @param context The application Context
+   * @return An intent that will display this Activity
    */
   public static Intent createViewIntent(Context context) {
     Intent intent = new Intent(context, SetPreferencesActivity.class);
@@ -17,9 +40,9 @@ public class SetPreferencesActivity extends PreferenceActivity {
     return intent;
   }
 
+  /** Called when this Activity is first created **/
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    // TODO Auto-generated method stub
     super.onCreate(savedInstanceState);
     addPreferencesFromResource(R.xml.preferences);
   }
